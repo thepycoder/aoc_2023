39fca281d6d6be2a788ef869bde4432cecf13462
Victor Sonck <victor.sonck@gmail.com>
2023-12-09 08:17:08 +0100

    ChatGPT refactor of correct code to make it cleaner and more readable

--- day9/day9.py
+++ day9/day9.py
@@ -1,34 +1,39 @@
-from typing import List
+from typing import List, Callable
 import numpy as np
 from numpy.typing import NDArray
 
 
-def part1(lines: List[str]):
+def process_line(line: str, calculate_total: Callable[[List[NDArray[np.int32]]], int]) -> int:
+    history: List[NDArray[np.int32]] = [np.array([int(value) for value in line.split()])]
+    while np.sum(history[-1]) != 0:
+        history.append(np.diff(history[-1]))
+
+    return calculate_total(history)
+
+def calculate_total_part1(history: List[NDArray[np.int32]]) -> int:
+    line_total = 0
+    for sequence in history[::-1]:  # Reverse the list of diff sequences
+        line_total += sequence[-1]
+    return line_total
+
+def calculate_total_part2(history: List[NDArray[np.int32]]) -> int:
+    line_total = 0
+    for sequence in history[::-1]:  # Reverse the list of diff sequences
+        line_total = -1 * (line_total - sequence[0])
+    return line_total
+
+def part1(lines: List[str]) -> int:
     total_sum = 0
     for line in lines:
-        history: List[NDArray[np.int32]] = [np.array([int(value) for value in line.split()])]
-        while np.sum(history[-1]) != 0:
-            history.append(np.diff(history[-1]))
-        
-        line_total = 0
-        for sequence in history[::-1]:  # Reverse the list of diff sequences
-            line_total += sequence[-1]
-        
+        line_total = process_line(line, calculate_total_part1)
         print(f"Line total: {line_total}")
         total_sum += line_total
     return total_sum
 
-def part2(lines: List[str]):
+def part2(lines: List[str]) -> int:
     total_sum = 0
     for line in lines:
-        history: List[NDArray[np.int32]] = [np.array([int(value) for value in line.split()])]
-        while np.sum(history[-1]) != 0:
-            history.append(np.diff(history[-1]))
-        
-        line_total = 0
-        for sequence in history[::-1]:  # Reverse the list of diff sequences
-            line_total = -1 * (line_total - sequence[0])
-        
+        line_total = process_line(line, calculate_total_part2)
         print(f"Line total: {line_total}")
         total_sum += line_total
     return total_sum
